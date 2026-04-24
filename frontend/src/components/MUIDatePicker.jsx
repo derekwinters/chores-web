6e03b17d338820c00a79ecba2b79a493b0d9a249
Derek Winters <derekmwinters@gmail.com>
2026-04-23 22:32:08 -0500

    fix: use StaticDatePicker instead of DatePicker to avoid duplication
    Replace DatePicker with StaticDatePicker to show only calendar view without
    input field. Eliminates duplication with existing next-due display and edit
    button in the form. Remove default action bar from picker.

--- frontend/src/components/MUIDatePicker.jsx
+++ frontend/src/components/MUIDatePicker.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DatePicker } from "@mui/x-date-pickers";
+import { StaticDatePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
@@ -9,7 +9,6 @@ export default function MUIDatePicker({ initialDate, onSelect, onCancel }) {
   const [value, setValue] = React.useState(initialDate ? dayjs(initialDate) : dayjs());
 
   const handleChange = (newValue) => {
-    setValue(newValue);
     if (newValue) {
       const dateStr = newValue.format("YYYY-MM-DD");
       onSelect(dateStr);
@@ -19,12 +18,12 @@ export default function MUIDatePicker({ initialDate, onSelect, onCancel }) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div className="mui-date-picker-wrapper">
-        <DatePicker
+        <StaticDatePicker
           value={value}
           onChange={handleChange}
           slotProps={{
-            popper: {
-              placement: "bottom-start",
+            actionBar: {
+              actions: [],
             },
           }}
         />
